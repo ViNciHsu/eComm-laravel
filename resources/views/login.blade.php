6b1b2b749804d303d7d25835fddd5689579db181
ViNciHsu <ViNciHsu>
2020-11-01 17:06:02 +0800

    簡易 header,footer,login 完成

---
+++ resources/views/login.blade.php
@@ -0,0 +1,20 @@
+@extends('master')
+@section('content')
+<div class="container custom-login">
+    <div class="row">
+        <div class="col-sm-4 col-sm-offset-4">
+            <form>
+                <div class="form-group">
+                    <label for="exampleInputEmail">Email address</label>
+                    <input type="email" class="form-control" id="exampleInputEmail" placeholder="Search">
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputEmail">Email address</label>
+                    <input type="email" class="form-control" id="exampleInputEmail" placeholder="Search">
+                </div>
+                <button type="submit" class="btn btn-default">Submit</button>
+            </form>
+        </div>
+    </div>
+</div>
+@endsection
