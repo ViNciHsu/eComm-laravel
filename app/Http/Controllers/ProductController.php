e772719bca14d73c894019b058f0aae17aac4228
ViNciHsu <ViNciHsu>
2020-11-03 16:03:53 +0800

    add count to cart()

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Product;
+use App\Models\Cart;
+use Session;
+
 class ProductController extends Controller
 {
     //
@@ -26,5 +29,29 @@ class ProductController extends Controller
         return view('search',[
             'products' => $data
         ]);
+    }
+
+    function addToCart(Request $request)
+    {
+        if($request->session()->has('user'))
+        {
+            $cart = new Cart;
+            $cart->user_id = $request->session()->get('user')['id'];
+            $cart->product_id = $request->product_id;
+            $cart->save();
+
+            return redirect('/');
+        }
+        else
+        {
+            return redirect('/login');
+        }
+
+    }
+
+    static function cartItem()
+    {
+        $userId = Session::get('user')['id'];
+        return Cart::where('user_id',$userId)->count();
     }
 }
